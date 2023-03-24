d3cad65f989d3bb1b9e33e510312cd687b5fd07e
Jacqueline Martin <Jsmartin09@gmail.com>
2023-03-23 21:41:50 -0400

    ran build

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/random_quote_machine", 
+  build: {
+    outDir: 'docs'
+  }
 })
